3345a890fc32b6b32efb59b6b4bbb35104c62320
Arman Kurtovic <armankurtovic01@gmail.com>
2024-08-13 12:12:55 +0200

    Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Root path
+  root to: 'tasks#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # RESTful routes for tasks except show
+  resources :tasks, except: [:show]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Custom show route with a different name
+  get 'tasks/:id', to: 'tasks#show', as: :show_task
 end
